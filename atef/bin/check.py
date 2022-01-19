210a02e4e702354b55eac09858a754e33bef4e7c
Ken Lauer <klauer@slac.stanford.edu>
2022-01-19 09:47:59 -0800

    MNT: split out per-device logging

--- atef/bin/check.py
+++ atef/bin/check.py
@@ -4,15 +4,16 @@
 
 import argparse
 import logging
-from typing import Generator, List, Optional, Sequence, Tuple
+from typing import Dict, Generator, List, Optional, Sequence, Tuple, Union
 
 import apischema
 import happi
 import ophyd
 import yaml
 
-from ..check import (Configuration, ConfigurationFile, Result, Severity,
-                     check_device, pv_config_to_device_config)
+from ..check import (ConfigurationFile, DeviceConfiguration, PVConfiguration,
+                     Result, Severity, check_device,
+                     pv_config_to_device_config)
 from ..util import ophyd_cleanup
 
 logger = logging.getLogger(__name__)
@@ -44,10 +45,13 @@ def build_arg_parser(argparser=None):
     return argparser
 
 
+AnyConfiguration = Union[PVConfiguration, DeviceConfiguration]
+
+
 def check_config_file(
     config: ConfigurationFile,
     filtered_devices: Optional[Sequence[str]] = None
-) -> Generator[Tuple[ophyd.Device, Configuration, Severity, List[Result]], None, None]:
+) -> Generator[Tuple[ophyd.Device, AnyConfiguration, Severity, List[Result]], None, None]:
     for pv_config in config.pvs:
         if filtered_devices and pv_config.name not in filtered_devices:
             logger.debug(
@@ -97,6 +101,40 @@ def check_config_file(
         yield (dev, dev_config, severity, results)
 
 
+def log_results(
+    device: ophyd.Device,
+    severity: Severity,
+    config: Union[PVConfiguration, DeviceConfiguration],
+    results: List[Result],
+    *,
+    severity_to_log_level: Optional[Dict[Severity, int]] = None,
+):
+    """Log check results to the module logger."""
+    severity_to_log_level = severity_to_log_level or default_severity_to_log_level
+
+    passed_or_failed = "passed" if severity <= Severity.warning else "FAILED"
+    logger.info(
+        "Device %s (%s) %s with severity %s",
+        device.name,
+        config.description or "no description",
+        passed_or_failed,
+        severity.name,
+    )
+    for result in results:
+        log_level = severity_to_log_level[result.severity]
+        if not logger.isEnabledFor(log_level):
+            continue
+        logger.log(log_level, result.reason)
+
+
+default_severity_to_log_level = {
+    Severity.success: logging.DEBUG,
+    Severity.warning: logging.WARNING,
+    Severity.error: logging.ERROR,
+    Severity.internal_error: logging.ERROR,
+}
+
+
 def main(
     filename: str,
     filtered_devices: Optional[Sequence[str]] = None,
@@ -106,29 +144,16 @@ def main(
     serialized_config = yaml.safe_load(open(filename))
     config = apischema.deserialize(ConfigurationFile, serialized_config)
 
-    severity_to_log_level = {
-        Severity.success: logging.DEBUG,
-        Severity.warning: logging.WARNING,
-        Severity.error: logging.ERROR,
-        Severity.internal_error: logging.ERROR,
-    }
-
-    for dev, config, severity, results in check_config_file(
-        config, filtered_devices=filtered_devices
-    ):
-        passed_or_failed = "passed" if severity <= Severity.warning else "FAILED"
-        logger.info(
-            "Device %s (%s) %s with severity %s",
-            dev.name,
-            config.description or "no description",
-            passed_or_failed,
-            severity.name,
-        )
-        for result in results:
-            log_level = severity_to_log_level[result.severity]
-            if not logger.isEnabledFor(log_level):
-                continue
-            logger.log(log_level, result.reason)
-
-    if cleanup:
-        ophyd_cleanup()
+    try:
+        for dev, config, severity, results in check_config_file(
+            config, filtered_devices=filtered_devices
+        ):
+            log_results(
+                device=dev,
+                config=config,
+                severity=severity,
+                results=results,
+            )
+    finally:
+        if cleanup:
+            ophyd_cleanup()
