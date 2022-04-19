b6efe29872e977596360e0c2007e4a6700d190de
Ken Lauer <klauer@slac.stanford.edu>
2022-04-19 16:46:20 -0700

    TST: add quick check for atef.bin

--- atef/bin/__init__.py
+++ atef/bin/__init__.py
@@ -1,3 +1,3 @@
-from .main import main
+from . import main
 
 __all__ = ["main"]
---
+++ atef/tests/test_commandline.py
@@ -0,0 +1,24 @@
+import sys
+
+import pytest
+
+import atef.bin.main as atef_main
+
+# from atef.bin.config import main as config_main
+
+
+def test_help_main(monkeypatch):
+    monkeypatch.setattr(sys, 'argv', ['--help'])
+    atef_main.main()
+
+
+@pytest.mark.parametrize('subcommand', list(atef_main.COMMANDS))
+def test_help_module(monkeypatch, subcommand):
+    monkeypatch.setattr(sys, 'argv', [subcommand, '--help'])
+    with pytest.raises(SystemExit):
+        atef_main.main()
+
+
+def test_check_smoke():
+    from atef.bin.check import main as check_main
+    check_main(filename="")
