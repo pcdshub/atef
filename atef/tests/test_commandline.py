c3385ad0422d557b6f1b365ea83c4bbd845f7430
Ken Lauer <klauer@slac.stanford.edu>
2022-04-20 11:20:22 -0700

    TST: use mock signal cache

--- atef/tests/test_commandline.py
+++ atef/tests/test_commandline.py
@@ -4,7 +4,9 @@ import pytest
 
 import atef.bin.main as atef_main
 
+from .. import check
 from .conftest import CONFIG_PATH
+from .test_comparison_device import mock_signal_cache  # noqa: F401
 
 
 def test_help_main(monkeypatch):
@@ -19,6 +21,7 @@ def test_help_module(monkeypatch, subcommand):
         atef_main.main()
 
 
-def test_check_smoke():
+def test_check_smoke(monkeypatch, mock_signal_cache):  # noqa: F811
     from atef.bin.check import main as check_main
+    monkeypatch.setattr(check, "get_signal_cache", lambda: mock_signal_cache)
     check_main(filename=str(CONFIG_PATH / "pv_based.yml"))
