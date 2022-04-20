5718a5f3a28b8ce551d06b80e561d0dbc9b0e647
Ken Lauer <klauer@slac.stanford.edu>
2022-04-20 11:58:20 -0700

    TST: fix up monkeypatching of device-based atef.bin.check

--- atef/tests/test_commandline.py
+++ atef/tests/test_commandline.py
@@ -4,7 +4,7 @@ import pytest
 
 import atef.bin.main as atef_main
 
-from .. import check, util
+from .. import check
 from .conftest import CONFIG_PATH
 from .test_comparison_device import at2l0, mock_signal_cache  # noqa: F401
 
@@ -28,6 +28,10 @@ def test_check_pv_smoke(monkeypatch, mock_signal_cache):  # noqa: F811
 
 
 def test_check_device_smoke(monkeypatch, at2l0):  # noqa: F811
-    from atef.bin.check import main as check_main
-    monkeypatch.setattr(util, "get_happi_device_by_name", lambda: at2l0)
-    check_main(filename=str(CONFIG_PATH / "device_based.yml"))
+    from atef.bin import check as bin_check
+
+    def get_happi_device_by_name(name, client=None):
+        return at2l0
+
+    monkeypatch.setattr(bin_check, "get_happi_device_by_name", get_happi_device_by_name)
+    bin_check.main(filename=str(CONFIG_PATH / "device_based.yml"))
