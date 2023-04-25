2b4f802e8ed76242361c0c95b7300a782cdbcce7
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2023-04-25 13:25:10 -0700

    ENH: allow Qt args on the command line

--- atef/bin/config.py
+++ atef/bin/config.py
@@ -3,10 +3,11 @@
 """
 import argparse
 import logging
+import sys
 from typing import List, Optional
 
 from pydm import exception
-from qtpy.QtWidgets import QApplication
+from qtpy.QtWidgets import QApplication, QStyleFactory
 
 from ..type_hints import AnyPath
 from ..widgets.config.window import Window
@@ -17,6 +18,39 @@ logger = logging.getLogger(__name__)
 def build_arg_parser(argparser=None):
     if argparser is None:
         argparser = argparse.ArgumentParser()
+
+    # Arguments that need to be passed through to Qt
+    qt_args = {
+        '-qmljsdebugger': 1,
+        '-reverse': '?',
+        '-stylesheet': 1,
+        '-widgetcount': '?',
+        '-platform': 1,
+        '-platformpluginpath': 1,
+        '-platformtheme': 1,
+        '-plugin': 1,
+        '-qwindowgeometry': 1,
+        '-qwindowicon': 1,
+        '-qwindowtitle': 1,
+        '-session': 1,
+        '-display': 1,
+        '-geometry': 1
+    }
+
+    for name in qt_args:
+        argparser.add_argument(
+            name,
+            type=str,
+            nargs=qt_args[name]
+        )
+
+    argparser.add_argument(
+        '-style',
+        type=str,
+        choices=QStyleFactory.keys(),
+        default='fusion' if sys.platform == 'darwin' else None,
+        help='Qt style to use for the application'
+    )
 
     argparser.add_argument(
         "filenames",
@@ -29,8 +63,8 @@ def build_arg_parser(argparser=None):
     return argparser
 
 
-def main(filenames: Optional[List[AnyPath]] = None):
-    app = QApplication([])
+def main(filenames: Optional[List[AnyPath]] = None, **kwargs):
+    app = QApplication(sys.argv)
     main_window = Window(show_welcome=not filenames)
     main_window.show()
     exception.install()
