123bb1b96041996f03c734092a8241a933d4bd6b
tangkong <tangkongrobert@gmail.com>
2024-09-13 11:19:30 -0700

    BUG: store setpoint_pvname in Target where possible

--- atef/widgets/config/data_active.py
+++ atef/widgets/config/data_active.py
@@ -874,8 +874,9 @@ class TargetEntryWidget(DesignerDisplay, QtWidgets.QWidget):
         dot_attr = attr.attr
         _attr = '_' + dot_attr.replace('.', '_')
         dev_name = full_name[:-len(_attr)]
+        write_pv = getattr(attr.signal, 'setpoint_pvname', attr.pvname)
 
-        return Target(device=dev_name, attr=dot_attr, pv=attr.pvname)
+        return Target(device=dev_name, attr=dot_attr, pv=write_pv)
 
 
 class ActionRowWidget(TargetRowWidget):
