4dc6f5021fad8f26b4a8b3a2deaa3bcfb9a3fa74
tangkong <tangkongrobert@gmail.com>
2025-10-06 13:49:04 -0700

    MNT: add generic load_file helper for use across the library.  In the future consider refactoring this and the other dataclasses into a submodule

--- atef/__init__.py
+++ atef/__init__.py
@@ -1,3 +1,24 @@
-from .version import __version__  # noqa: F401
+from typing import Union
 
-__all__ = []
+from apischema import ValidationError
+
+from atef.config import ConfigurationFile
+from atef.procedure import ProcedureFile
+from atef.type_hints import AnyPath
+from atef.version import __version__  # noqa: F401
+
+
+def load_file(filepath: AnyPath) -> Union[ConfigurationFile, ProcedureFile]:
+    try:
+        data = ConfigurationFile.from_filename(filepath)
+    except ValidationError:
+        try:
+            data = ProcedureFile.from_filename(filepath)
+        except ValidationError:
+            raise ValueError(f'failed to open file ({filepath}) as either active '
+                             'or passive checkout')
+
+    return data
+
+
+__all__ = ["load_file"]
